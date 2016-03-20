bc40bbee8747a2202d65a606895fbad1eed08c27
oat9002 <oat9002@hotmail.com>
2016-03-21 01:10:09 +0700

    commit changed that I dont know.

--- App4/AddInfo2.xaml.cs
+++ App4/AddInfo2.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -37,58 +37,27 @@ namespace App4
 
         }
 
-        private void submit_Click(object sender, RoutedEventArgs e)
+        public void clearForm()
         {
-            User user = new User(name.Text,Int32.Parse(ID.Text),111,Int32.Parse(Whistband.Text));
-            user.save();
+            name.Text = "";
+            identifier.Text = "";
+            wristband.Text = "";
+        }
 
+        private async void save_Click(object sender, RoutedEventArgs e)
+        {
+            if(name.Text == "" || wristband.Text == "" || identifier.Text == "")
+            {
+                MessageDialog ms = new MessageDialog("Please fill the box below.");
+                await ms.ShowAsync();
+                clearForm();
+            }
+            else
+            {
+                User user = new User();
+                user.save();
+                clearForm();
+            }
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace App4
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class AddInfo2 : Page
-    {
-        public AddInfo2()
-        {
-            this.InitializeComponent();
-        }
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            String xname = name.Text;
-            String xID = ID.Text;
-            String xwhistband = Whistband.Text;
-        }
-    }
-}
->>>>>>> Add_Map
