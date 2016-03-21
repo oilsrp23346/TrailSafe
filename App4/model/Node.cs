e88b9476d020c69586b74fe9ef34d79a435ff208
oat9002 <oat9002@hotmail.com>
2016-03-22 01:57:46 +0700

    [#4]add getAllOfflineNode and getAllOnlineNode

--- App4/model/Node.cs
+++ App4/model/Node.cs
@@ -94,5 +94,77 @@ namespace App4.model
             Node node = new Node(id_pri, nodeType, onlineStatus, latitude, longitude);
             return node;
         }
+
+        public static Node[] getAllOfflineNode()
+        {
+            ArrayList nodeArr = new ArrayList();
+            Uri uri = new Uri("http://207.46.230.196/node/getofflinenodes");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(uri).Result;
+            JsonArray jsonArr = JsonValue.Parse(response.Content.ReadAsStringAsync().Result.ToString()).GetArray();
+            for (uint i = 0; i < jsonArr.Count; i++)
+            {
+                int id = -1;
+                int onlineStatus = -1;
+                string nodeType = "";
+                double latitude = -1;
+                double longitude = -1;
+                if (jsonArr.GetObjectAt(i)["id"].ValueType != JsonValueType.Null)
+                    id = Int32.Parse(jsonArr.GetObjectAt(i).GetNamedString("id"));
+                if (jsonArr.GetObjectAt(i)["node_type"].ValueType != JsonValueType.Null)
+                    nodeType = jsonArr.GetObjectAt(i).GetNamedString("node_type");
+                if (jsonArr.GetObjectAt(i)["online_status"].ValueType != JsonValueType.Null)
+                    onlineStatus = Int32.Parse(jsonArr.GetObjectAt(i).GetNamedString("online_status"));
+                if (jsonArr.GetObjectAt(i)["latitude"].ValueType != JsonValueType.Null)
+                    latitude = Double.Parse(jsonArr.GetObjectAt(i).GetNamedString("latitude"));
+                if (jsonArr.GetObjectAt(i)["longitude"].ValueType != JsonValueType.Null)
+                    longitude = Double.Parse(jsonArr.GetObjectAt(i).GetNamedString("longitude"));
+                Node node = new Node(id, nodeType, onlineStatus, latitude, longitude);
+                nodeArr.Add(node);
+            }
+            Node[] nodeReturn = new Node[nodeArr.Count];
+            int index = 0;
+            foreach (Node node in nodeArr)
+            {
+                nodeReturn[index++] = node;
+            }
+            return nodeReturn;
+        }
+
+        public static Node[] getAllOnlineNode()
+        {
+            ArrayList nodeArr = new ArrayList();
+            Uri uri = new Uri("http://207.46.230.196/node/getonlinenodes");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(uri).Result;
+            JsonArray jsonArr = JsonValue.Parse(response.Content.ReadAsStringAsync().Result.ToString()).GetArray();
+            for (uint i = 0; i < jsonArr.Count; i++)
+            {
+                int id = -1;
+                int onlineStatus = -1;
+                string nodeType = "";
+                double latitude = -1;
+                double longitude = -1;
+                if (jsonArr.GetObjectAt(i)["id"].ValueType != JsonValueType.Null)
+                    id = Int32.Parse(jsonArr.GetObjectAt(i).GetNamedString("id"));
+                if (jsonArr.GetObjectAt(i)["node_type"].ValueType != JsonValueType.Null)
+                    nodeType = jsonArr.GetObjectAt(i).GetNamedString("node_type");
+                if (jsonArr.GetObjectAt(i)["online_status"].ValueType != JsonValueType.Null)
+                    onlineStatus = Int32.Parse(jsonArr.GetObjectAt(i).GetNamedString("online_status"));
+                if (jsonArr.GetObjectAt(i)["latitude"].ValueType != JsonValueType.Null)
+                    latitude = Double.Parse(jsonArr.GetObjectAt(i).GetNamedString("latitude"));
+                if (jsonArr.GetObjectAt(i)["longitude"].ValueType != JsonValueType.Null)
+                    longitude = Double.Parse(jsonArr.GetObjectAt(i).GetNamedString("longitude"));
+                Node node = new Node(id, nodeType, onlineStatus, latitude, longitude);
+                nodeArr.Add(node);
+            }
+            Node[] nodeReturn = new Node[nodeArr.Count];
+            int index = 0;
+            foreach (Node node in nodeArr)
+            {
+                nodeReturn[index++] = node;
+            }
+            return nodeReturn;
+        }
     }
 }
