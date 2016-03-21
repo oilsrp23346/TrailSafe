e3bd9a71ca0cbe4f3693b389fc39052ec5fdcb99
oilsrp23346 <oilsrp23346@gmail.com>
2016-03-21 23:49:54 +0700

    push_pin

--- App4/map.xaml.cs
+++ App4/map.xaml.cs
@@ -19,6 +19,7 @@ using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Shapes;
 using Windows.Storage.Streams;
 using Windows.Devices.Geolocation;
+using Windows.UI;
 
 
 
@@ -33,32 +34,71 @@ namespace App4
     {
         RandomAccessStreamReference mapIconStreamReference;
         public map()
-        { 
+        {
             this.InitializeComponent();
             myMap.Loaded += MyMap_Loaded;
-            mapIconStreamReference = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///image/Pin.png"));
+            mapIconStreamReference = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///image/mappin.png"));
         }
         private void MyMap_Loaded(object sender, RoutedEventArgs e)
         {
-            myMap.Center =
-               new Geopoint(new BasicGeoposition()
-               {
-                   //Geopoint for Seattle 
-                   Latitude = 16.734522,
-                   Longitude = 102.285402
-               });
-            myMap.ZoomLevel = 13;
-            //////////////Seattle/////////////
-            this.myMap.Style = MapStyle.AerialWithRoads;
+            
+            BasicGeoposition snPosition = new BasicGeoposition() { Latitude = 16.734522, Longitude = 102.285402 };
+            Geopoint snPoint = new Geopoint(snPosition);
 
+            // Create a MapIcon.
             MapIcon mapIcon1 = new MapIcon();
-            mapIcon1.Location = myMap.Center;
-            // mapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+            mapIcon1.Location = snPoint;
+            mapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
             mapIcon1.Title = "Center";
             mapIcon1.Image = mapIconStreamReference;
             mapIcon1.ZIndex = 0;
+
+            // Add the MapIcon to the map.
             myMap.MapElements.Add(mapIcon1);
 
+            // Center the map over the POI.
+            myMap.Center = snPoint;
+            myMap.ZoomLevel = 13;
+            /*
+                        myMap.Center =
+                           new Geopoint(new BasicGeoposition()
+                           {
+                               //Geopoint for Seattle 
+                               Latitude = 16.734522,
+                               Longitude = 102.285402
+                           });
+                        myMap.ZoomLevel = 13;
+                        //////////////Seattle/////////////
+                        this.myMap.Style = MapStyle.AerialWithRoads;
+
+                        MapIcon mapIcon1 = new MapIcon();
+                        mapIcon1.Location = myMap.Center;
+                        // mapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+                        mapIcon1.Title = "Center";
+                        mapIcon1.Image = mapIconStreamReference;
+                        mapIcon1.ZIndex = 0;
+                        myMap.MapElements.Add(mapIcon1);
+                        */
+            //line
+            double centerLatitude = myMap.Center.Position.Latitude;
+            double centerLongitude = myMap.Center.Position.Longitude;
+            double centerLatitude2 = myMap.Center.Position.Latitude;
+            double centerLongitude2 = myMap.Center.Position.Longitude;
+            Windows.UI.Xaml.Controls.Maps.MapPolyline mapPolyline = new Windows.UI.Xaml.Controls.Maps.MapPolyline();
+            mapPolyline.Path = new Geopath(new List<BasicGeoposition>() {
+         new BasicGeoposition() {Latitude=centerLatitude, Longitude=centerLongitude },
+         new BasicGeoposition() {Latitude=centerLatitude+0.05, Longitude=centerLongitude-0.05 },
+
+         new BasicGeoposition() {Latitude=centerLatitude2+0.05, Longitude=centerLongitude2-0.05 },
+         new BasicGeoposition() {Latitude=centerLatitude2+0.05, Longitude= centerLongitude2+0.0000005},
+
+         new BasicGeoposition() {Latitude=centerLatitude2+0.05, Longitude=centerLongitude2-0.0000005 },
+         new BasicGeoposition() {Latitude=centerLatitude2, Longitude= centerLongitude2},
+                  });
+            mapPolyline.StrokeColor = Colors.White;
+            mapPolyline.StrokeThickness = 3;
+            mapPolyline.StrokeDashed = true;
+            myMap.MapElements.Add(mapPolyline);
         }
     }
 }
