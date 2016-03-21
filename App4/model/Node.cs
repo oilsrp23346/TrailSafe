a733a4e0ded3dedb032b19ff28151a360b75009d
oat9002 <oat9002@hotmail.com>
2016-03-22 01:41:40 +0700

    [#4]add getNodeByID

--- App4/model/Node.cs
+++ App4/model/Node.cs
@@ -50,13 +50,13 @@ namespace App4.model
                 double longitude = -1;
                 if(jsonArr.GetObjectAt(i)["id"].ValueType != JsonValueType.Null)
                     id = Int32.Parse(jsonArr.GetObjectAt(i).GetNamedString("id"));
-                if (jsonArr.GetObjectAt(i)["node_type"].ValueType != JsonValueType.Null)
+                if(jsonArr.GetObjectAt(i)["node_type"].ValueType != JsonValueType.Null)
                     nodeType = jsonArr.GetObjectAt(i).GetNamedString("node_type");
-                if (jsonArr.GetObjectAt(i)["online_status"].ValueType != JsonValueType.Null)
+                if(jsonArr.GetObjectAt(i)["online_status"].ValueType != JsonValueType.Null)
                     onlineStatus = Int32.Parse(jsonArr.GetObjectAt(i).GetNamedString("online_status"));
-                if (jsonArr.GetObjectAt(i)["latitude"].ValueType != JsonValueType.Null)
+                if(jsonArr.GetObjectAt(i)["latitude"].ValueType != JsonValueType.Null)
                     latitude = Double.Parse(jsonArr.GetObjectAt(i).GetNamedString("latitude"));
-                if (jsonArr.GetObjectAt(i)["longitude"].ValueType != JsonValueType.Null)
+                if(jsonArr.GetObjectAt(i)["longitude"].ValueType != JsonValueType.Null)
                     longitude = Double.Parse(jsonArr.GetObjectAt(i).GetNamedString("longitude"));
                 Node node = new Node(id, nodeType, onlineStatus, latitude, longitude);
                 nodeArr.Add(node);
@@ -68,6 +68,31 @@ namespace App4.model
                 nodeReturn[index++] = node;
             }
             return nodeReturn;
+        }
+
+        public static Node getNodeByID(int id)
+        {
+            Uri uri = new Uri("http://207.46.230.196/node/find_by_id?device-id=" + id);
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(uri).Result;
+            JsonValue json = JsonValue.Parse(response.Content.ReadAsStringAsync().Result.ToString());
+            int id_pri = -1;
+            int onlineStatus = -1;
+            string nodeType = "";
+            double latitude = -1;
+            double longitude = -1;
+            if(json.GetObject()["id"].ValueType != JsonValueType.Null)
+                id_pri = Int32.Parse(json.GetObject().GetNamedString("id"));
+            if(json.GetObject()["node_type"].ValueType != JsonValueType.Null)
+                nodeType = json.GetObject().GetNamedString("node_type");
+            if(json.GetObject()["online_status"].ValueType != JsonValueType.Null)
+                onlineStatus = Int32.Parse(json.GetObject().GetNamedString("online_status"));
+            if (json.GetObject()["latitude"].ValueType != JsonValueType.Null)
+                latitude = Double.Parse(json.GetObject().GetNamedString("latitude"));
+            if(json.GetObject()["longitude"].ValueType != JsonValueType.Null)
+                longitude = Double.Parse(json.GetObject().GetNamedString("longitude"));
+            Node node = new Node(id_pri, nodeType, onlineStatus, latitude, longitude);
+            return node;
         }
     }
 }
