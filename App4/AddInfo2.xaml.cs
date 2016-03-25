698c6613ea5c1329fa1eda46e2636babd0df7a5c
oat9002 <oat9002@hotmail.com>
2016-03-26 04:43:13 +0700

    fix conflict

--- App4/AddInfo2.xaml.cs
+++ App4/AddInfo2.xaml.cs
@@ -1,52 +1,5 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace App4
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class AddInfo2 : Page
-    {
-        public AddInfo2()
-        {
-            this.InitializeComponent();
-        }
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void submit_Click(object sender, RoutedEventArgs e)
-        {
-            User user = new User(name.Text,Int32.Parse(ID.Text),111,Int32.Parse(Whistband.Text));
-            user.save();
-
-        }
-    }
-}
-=======
-﻿using App4.Model;
+using App4.Model;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -110,4 +63,3 @@ namespace App4
         }
     }
 }
->>>>>>> 363f1be3564d45b1eff5b15632f0bede15c909b3
