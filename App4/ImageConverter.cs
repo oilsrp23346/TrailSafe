0acdddaa374cb53d016cdebdcbe600f06b4a08f6
oilsrp23346 <oilsrp23346@gmail.com>
2016-03-30 02:28:54 +0700

    edit la lo

---
+++ App4/ImageConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+
+namespace App4.model
+{
+    public class ImageConverter
+    {
+        public static async Task<byte[]> BitmapToByteArray(IRandomAccessStream stream)
+        {
+            BitmapDecoder decoder =  await BitmapDecoder.CreateAsync(stream);
+            PixelDataProvider pixelData = await decoder.GetPixelDataAsync();
+            byte[] bytes = pixelData.DetachPixelData();
+            return bytes;
+        }
+
+        public static string ByteArrayToBase64(byte[] buffer)
+        {
+            string base64 = Convert.ToBase64String(buffer);
+            return base64;
+        }
+
+        public static byte[] Base64ToByteArray(string base64)
+        {
+            byte[] buffer = Convert.FromBase64String(base64);
+            return buffer;
+        }
+    }
+}
